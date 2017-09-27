0c8c079933c81a37af9cf3ce30ff071d5769ff6b
leonkuczma <kstolarczyk89@gmail.com>
2017-09-27 19:30:10 +0200

    Create ConvertToDataTable.cs

---
+++ ConvertToDataTable.cs
@@ -0,0 +1,42 @@
+//Wersja na VB
+Public Function ConvertToDataTable(Of T)(data As IList(Of T)) As DataTable
+        Dim properties As PropertyDescriptorCollection = TypeDescriptor.GetProperties(GetType(T))
+        Dim table As New DataTable()
+        For Each prop As PropertyDescriptor In properties
+            table.Columns.Add(prop.Name, If(Nullable.GetUnderlyingType(prop.PropertyType), prop.PropertyType))
+        Next
+        For Each item As T In data
+            Dim row As DataRow = table.NewRow()
+            For Each prop As PropertyDescriptor In properties
+                row(prop.Name) = If(prop.GetValue(item), DBNull.Value)
+            Next
+            table.Rows.Add(row)
+        Next
+        Return table
+
+    End Function
+   
+   
+//Wersja C#
+
+ public DataTable ConvertToDataTable<T>(IList<T> data)
+    {
+        PropertyDescriptorCollection properties =
+           TypeDescriptor.GetProperties(typeof(T));
+        DataTable table = new DataTable();
+        foreach (PropertyDescriptor prop in properties)
+            table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+        foreach (T item in data)
+        {
+            DataRow row = table.NewRow();
+            foreach (PropertyDescriptor prop in properties)
+                row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+            table.Rows.Add(row);
+        }
+        return table;
+
+    }
+   
+   
+   
+   
